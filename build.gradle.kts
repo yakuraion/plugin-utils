ab90837b431cdd63f9956bb097d5b0b3f3dda57b
Vyacheslav Yakovlev <yakuraion@gmail.com>
2022-08-26 22:54:32 +0300

    add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,15 @@
 plugins {
     `kotlin-dsl`
+    `maven-publish`
 }
 
 group = "pro.yakuraion.plugins.utils"
 version = "1.0-SNAPSHOT"
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
