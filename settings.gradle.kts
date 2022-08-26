41573ee3f28c4d5ce1d4f264df4f870f659a85aa
Vyacheslav Yakovlev <yakuraion@gmail.com>
2022-08-26 22:54:21 +0300

    fix dependencyResolutionManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,13 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
+        google()
+        mavenCentral()
     }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
