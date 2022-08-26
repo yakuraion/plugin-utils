aea289bf0d6b417d0bd0582ded5b0f7fece8c71e
Vyacheslav Yakovlev <yakuraion@gmail.com>
2022-08-26 22:53:56 +0300

    add OSUtils

---
+++ src/main/kotlin/pro/yakuraion/plugins/utils/OSUtils.kt
@@ -0,0 +1,18 @@
+package pro.yakuraion.plugins.utils
+
+import org.gradle.api.Project
+import org.gradle.internal.os.OperatingSystem
+import java.io.File
+
+fun Project.openFileByOS(file: File) {
+    val currentOS = OperatingSystem.current()
+    val args = when {
+        currentOS.isMacOsX -> listOf("open")
+        currentOS.isLinux -> listOf("xdg-open")
+        currentOS.isWindows -> listOf("cmd", "/c", "start")
+        else -> throw Exception("Unsupported OS: ${currentOS.name}")
+    } + listOf(file.absolutePath)
+    exec {
+        commandLine(args)
+    }
+}
